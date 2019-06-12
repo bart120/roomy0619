1ba2d102f8dbfa73fe632cd048e2dace2bf6c3d1
Vincent <vleclerc@inow.fr>
2019-06-12 16:02:46 +0200

    ajout des icones material

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
     imports: [
         BrowserAnimationsModule,
         MatToolbarModule,
-        MatButtonModule
+        MatButtonModule,
+        MatIconModule
     ],
     exports: [
         BrowserAnimationsModule,
         MatToolbarModule,
-        MatButtonModule]
+        MatButtonModule,
+        MatIconModule]
 })
 export class MaterialModule { }
