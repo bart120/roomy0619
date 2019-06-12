4d09875883eb39b7a12e44ea84551c40e1511f1f
Vincent <vleclerc@inow.fr>
2019-06-12 16:58:04 +0200

    directive et menu

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
     imports: [
         BrowserAnimationsModule,
         MatToolbarModule,
         MatButtonModule,
-        MatIconModule
+        MatIconModule,
+        MatMenuModule
     ],
     exports: [
         BrowserAnimationsModule,
         MatToolbarModule,
         MatButtonModule,
-        MatIconModule]
+        MatIconModule,
+        MatMenuModule]
 })
 export class MaterialModule { }
